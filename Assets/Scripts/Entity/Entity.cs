71f630f0e7e1c9f040c267ebe11ef598f60b9590
devinrshaw <devinrshaw@gmail.com>
2025-01-21 21:34:33 -0800

    Class Outlines
    The abstract classes for entites, hands and projectiles. With the dervied classes as well. All of this is outline and needs to be reviewed.

---
+++ Assets/Scripts/Entity/Entity.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    // Attributes
+    protected int health;            // Representing the health attribute.
+    protected Hand entityHand;       // Representing the entityHand attribute.
+
+    // Method to take damage
+    public void TakeHit(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Virtual method for attack, can be overridden
+    public virtual void Attack()
+    {
+        // Logic for attack (to be overridden in derived classes)
+    }
+
+    // Virtual method for death, can be overridden
+    public virtual void Die()
+    {
+        // Logic for death
+        Debug.Log("Entity died.");
+    }
+}
---
+++ Assets/Scripts/Hand/Hand.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+
+    // Array of cards as shown in UML
+    protected Card[] cards;
+
+    // Abstract method required by UML
+    public abstract Card[] GetHand()
+    {
+        return cards;
+    }
+
+    // Protected constructor if needed
+    protected Hand()
+    {
+        // Initialize the cards array
+        cards = new Card[0];
+    }
+
+    // Optional: Common functionality for all hands
+    protected bool IsValidIndex(int index)
+    {
+        return index >= 0 && index < cards.Length;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
