b64d5d7e88e09bbc45569c01b0849bc697c0e9ef
devinrshaw <devinrshaw@gmail.com>
2025-01-25 20:00:40 -0800

    angles and stopping of enemy
    enemy will face in character direction and move towards character until close enough to attack

--- Assets/Scripts/Entity/Enemy.cs
+++ Assets/Scripts/Entity/Enemy.cs
@@ -3,15 +3,48 @@ using UnityEngine;
 public class Enemy : Entity
 {
 
+    //copied from intial player script 
+    private float moveSpeed = 2.0f;
+
+    Rigidbody2D enemyRb;
+
+    Transform playerTarget;
+    Vector2 moveDir;
+
+    private void Awake()
+    {
+        enemyRb = GetComponent<Rigidbody2D>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerTarget = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         
+        //tutorial had vector3 not sure why, change if creates issues 
+        Vector2 direction = (playerTarget.position - transform.position).normalized;
+        moveDir = direction;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        enemyRb.rotation = angle;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //logic for handling when close to a player, if close stand still 
+        float distanceToPlayer = (playerTarget.position - transform.position).magnitude;
+        if (playerTarget && distanceToPlayer > 2)
+        {
+            enemyRb.linearVelocity = moveDir * moveSpeed;
+        }
+
+        else
+        {
+            enemyRb.linearVelocity = Vector2.zero;
+        }
     }
 }
