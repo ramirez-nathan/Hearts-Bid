493720a54f5e65f0b5c915d9f9aa6a844d3b3af0
ramirez-nathan <nater0826@gmail.com>
2025-01-29 15:34:51 -0800

    Added Debug log for card object destroy

--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    private Transform target;
+    private Transform target; // enemy
     private float moveSpeed;
 
     private float distToTargetToDestroyProjectile = 1f;
@@ -15,16 +15,19 @@ public class Projectile : MonoBehaviour
     
     void Update()
     {
+        
+        
+        
+    }
+    void FixedUpdate()
+    {
         Vector3 moveDirNormalized = (target.position - transform.position).normalized;
         transform.position += moveDirNormalized * moveSpeed * Time.deltaTime;
         if (Vector3.Distance(transform.position, moveDirNormalized) < distToTargetToDestroyProjectile)
         {
             Destroy(gameObject);
+            Debug.Log("Destroyed Card");
         }
-    }
-    void FixedUpdate()
-    {
-
     }
 
     public void InitializeProjectile(Transform target, float moveSpeed)
