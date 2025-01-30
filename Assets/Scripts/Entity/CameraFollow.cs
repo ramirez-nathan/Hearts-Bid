ccbd29a2335f79336adb1dd22c4f5b483e53ee46
Michael Carlin <mrcarlin2003@gmail.com>
2025-01-30 14:01:38 -0800

    Implemented the camera following the player

---
+++ Assets/Scripts/Entity/CameraFollow.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] GameObject playerToFollow;
+
+    void LateUpdate()
+    {
+        transform.position = playerToFollow.transform.position + new Vector3(0, 0, -10); // need a z offset so camera isn't "in" player
+    }
+}
