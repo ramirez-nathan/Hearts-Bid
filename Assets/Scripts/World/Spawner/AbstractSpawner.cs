aa39e7999fbe47fa77809f09743c1ffc105ca02f
devinrshaw <devinrshaw@gmail.com>
2025-03-06 19:46:50 -0800

    Spawner Logic
    added the all around world logic of the spawners

--- Assets/Scripts/World/Spawner/AbstractSpawner.cs
+++ Assets/Scripts/World/Spawner/AbstractSpawner.cs
@@ -1,27 +1,58 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public abstract class AbstractSpawner : MonoBehaviour
 {
     protected abstract float spawnRate { get; }
-    protected abstract GameObject[] SpawnTypes { get; }
-    protected abstract int maxSpawn { get; }
+    protected abstract string spawnerTag { get; }
+
+    protected abstract string spawneeTag { get; } 
+
+    protected abstract GameObject[] spawnTypes { get; }
+
+
+
+    //tracks spawnees that are still alive 
+    protected List<GameObject> spawnsAlive;
+
+
+    protected int maxSpawn = 1;
 
     protected int numSpawned = 0;
 
-    bool isOnCooldown = false; 
-    
+    bool isOnCooldown = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int round = 0;
+
+    protected Transform[] spawnAreas;
+
+
+
+
     void Start()
     {
-        //not sure we need anything here 
+        // Get all game objects with the "Spawner" tag
+        GameObject[] spawners = GameObject.FindGameObjectsWithTag(spawnerTag);
+
+
+        // Create a list of spawn points based on their Transforms
+        spawnAreas = new Transform[spawners.Length];
+
+        // Populate the spawnAreas array with the Transforms of each spawner
+        for (int i = 0; i < spawners.Length; i++)
+        {
+            spawnAreas[i] = spawners[i].transform;
+        }
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
+
         
         if (!isOnCooldown && (numSpawned < maxSpawn))
         {
@@ -29,7 +60,16 @@ public abstract class AbstractSpawner : MonoBehaviour
             Spawn();
             StartCoroutine(WaitToSpawn());
         }
-        
+
+        spawnsAlive = new List<GameObject>(GameObject.FindGameObjectsWithTag(spawneeTag));
+
+
+        if (spawnsAlive.Count == 0)
+        {
+            round++;
+            numSpawned = 0;
+            maxSpawn += round * 5;
+        }
 
     }
 
@@ -41,4 +81,8 @@ public abstract class AbstractSpawner : MonoBehaviour
     }
     
     protected abstract void Spawn();
+
+
+
+
 }
---
+++ Assets/Scripts/World/Spawner/EnemySpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : AbstractSpawner
+{
+    //editable inherited attributes
+    [SerializeField] private float _spawnRate = 1.5f;
+    [SerializeField] private GameObject[] _spawnTypes;
+
+    protected override GameObject[] spawnTypes => _spawnTypes;
+    protected override float spawnRate => _spawnRate;
+
+
+    //tags specify where to spawn and what to monitor after spawn 
+    protected override string spawnerTag => "Spawner";
+    protected override string spawneeTag => "Enemy";
+
+
+
+
+
+
+
+    protected override void Spawn()
+    {
+        if (spawnTypes.Length == 0) return; // Safety check
+
+
+        // Pick a random prefab from the list
+        GameObject selectedPrefab = spawnTypes[Random.Range(0, spawnTypes.Length)];
+
+        //spawn at the area of the spawner 
+        Transform spawnPoint = spawnAreas[Random.Range(0, spawnAreas.Length)]; //change this to a list of spawn positions 
+
+        var spawn = Instantiate(selectedPrefab, spawnPoint.position, Quaternion.identity);
+
+    }
+
+
+
+}
